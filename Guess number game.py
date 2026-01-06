33dcd1a793d83acb4efba9048a356c03a9e71cf5
Isaiah-Zheng <zjz88060133@gmail.com>
2026-01-06 21:30:57 +0800

    add the diffculty setting and the debug the wrong hint, disappeared of heat and cold , and choose dificulty for 2 times.

--- Guess number game.py
+++ Guess number game.py
@@ -11,21 +11,39 @@ def Distance_Suggestion(UserAns,RanNum):
         print("Far")
     elif Dstans < 40:
         print("Very far")
+    else:
+        print("Very very far")
 
 def Mode_1():
     print("Hello,this is Guess number game, mode 1")
     count = 0
-    RanNum = random.randint(1,100)
     start_time = time.time()
-    time_limit = 20
     hint_det = 0
     hint_set = 1
 
+    # we need to set the difficulty here
+
+    dif = dificult_setting()
+    if dif == 1:
+        time_limit = 50
+        RanNum = random.randint(1,100)
+    elif dif == 2:
+        time_limit = 40
+        RanNum = random.randint(1,150)
+    elif dif == 3:
+        time_limit = 35
+        RanNum = random.randint(1,200)
+
+
     
     while True:
+        
+        # this is hint randomly generate part
         hint_det = random.randint(0,5)
         if hint_det == hint_set:
             hint(RanNum)
+        
+        # this is the time limitation part
         past_time = time.time() - start_time
         rest_time = int(time_limit - past_time)
         if past_time > time_limit:
@@ -76,7 +94,7 @@ def Mode_2():
             Distance_Suggestion(UserAns,RanNum)
     
 
-Mode_1()
+
 
 
 def hint(num):
@@ -89,22 +107,43 @@ def hint(num):
             print("It is odd")
 
     elif a == 2:
-        edge_1 = random.randint(1,30)
-        edge_2 = random.randint(1,15)
-        print(f"The answer is between [{num-edge_1},{num-edge_2}]")
+        edge_1 = random.randint(10,30)
+        edge_2 = random.randint(10,15)
+        print(f"The answer is between [{num-edge_1},{num+edge_2}]")
 
     
     elif a == 3:
-        edge_1 = random.randint(1,20)
-        edge_2 = random.randint(1,10)
-        print(f"The answer is between [{num-edge_1},{num-edge_2}]")
+        edge_1 = random.randint(10,20)
+        edge_2 = random.randint(5,10)
+        print(f"The answer is between [{num-edge_1},{num+edge_2}]")
 
     elif a == 4:
-        edge_1 = random.randint(1,15)
-        edge_2 = random.randint(1,15)
-        print(f"The answer is between [{num-edge_1},{num-edge_2}]")
+        edge_1 = random.randint(10,15)
+        edge_2 = random.randint(10,15)
+        print(f"The answer is between [{num-edge_1},{num+edge_2}]")
 
-    
+
+def dificult_setting():
+    while True:
+
+        print("")
+        print("1.Easy: 50s, range(0,100)")
+        print("2.Advanced: 40s, range(0,150)")
+        print("3.Difficult: 35s range(0,200)")
+        print("")
+        try:
+            det = int(input("Please choose the Difficulty you prefer >"))
+            break
+        except:
+            print("Error, input a number please")
+        
+        #(solve)maybe we need to check the validation of input latter
+
+        #(solved)why we need to choose again even if we input the valid 2 ?
+    return det
+
+#the end , why we have the sudden error when time is up?
+Mode_1()
 
             
 
